9ea18f7184f88e82e2bd3296d386a428cd89663e
Ricardo Alves Simão <ricardo.simao.25554@formandos.cinel.pt>
2022-09-06 16:01:12 +0100

    SeedDb

---
+++ Airlines25554/Airlines25554/Data/SeedDb.cs
@@ -0,0 +1,69 @@
+﻿using Airlines25554.Data.Entities;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Airlines25554.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context; // -> privado e readonly porque é apenas a ligação á base de dados
+
+        private Random _random;
+
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+
+            _random = new Random();
+        }
+
+        public async Task SeedAsync()
+        {
+            // Se a base de dados estiver criada nenhuma acção é tomada. Caso não exista, a base de dados é criada.
+            await _context.Database.EnsureCreatedAsync();
+
+            if (!_context.AirPlanes.Any())
+            {
+                AddAirplane();
+                await _context.SaveChangesAsync(); // Insere os aviões na base de dados
+            }
+        }
+
+        private void AddAirplane() // Verifica se a tabela de aviões tem algum avião, caso não tenha são inseridos aviões
+        {
+            _context.AirPlanes.Add(new AirPlane
+            {
+                Registration = "xx-22-ff",
+                Model = "DC-6",
+                EconomySeats = 56,
+                ExecutiveSeats = 8,
+                FirstClassSeats = 4
+             //   User = user,
+
+            });
+
+            _context.AirPlanes.Add(new AirPlane
+            {
+                Registration = "rr-22-ff",
+                Model = "Airbus A300",
+                EconomySeats = 99,
+                ExecutiveSeats = 5,
+                FirstClassSeats = 4
+                //  User = user,
+
+            });
+
+
+            _context.AirPlanes.Add(new AirPlane
+            {
+                Registration = "dd-11-ff",
+                Model = "Boeing 707",
+                EconomySeats = 99,
+                ExecutiveSeats = 11,
+                FirstClassSeats = 4
+                //   User = user,
+            });
+        }
+    }
+}
--- Airlines25554/Airlines25554/Startup.cs
+++ Airlines25554/Airlines25554/Startup.cs
@@ -1,15 +1,10 @@
 using Airlines25554.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Airlines25554
 {
@@ -29,6 +24,8 @@ namespace Airlines25554
             {
                 cfg.UseSqlServer(this.Configuration.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddTransient<SeedDb>(); 
 
             services.AddControllersWithViews();
         }
